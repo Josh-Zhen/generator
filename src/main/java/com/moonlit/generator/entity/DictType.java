01437dc86eca9be524594bf062e63ca0b32a8287
by丶Moonlit/2 <by.Moonlit@gmail.com>
2021-10-14 11:43:27 +0800

    更新代码

--- src/main/java/com/moonlit/generator/entity/DictType.java
+++ src/main/java/com/moonlit/generator/entity/DictType.java
@@ -15,45 +15,47 @@ import java.time.LocalDateTime;
  */
 @Data
 public class DictType implements Serializable {
+
     private static final long serialVersionUID = 1L;
+
     /**
      * 主键
      */
     private Long id;
+
     /**
      * 名称
      */
     private String name;
+
     /**
      * 编码
      */
     private String code;
+
     /**
      * 排序
      */
     private Integer sort;
+
     /**
      * 备注
      */
     private String remark;
+
     /**
      * 状态（字典 0停用 1正常 2删除）
      */
     private Integer status;
+
     /**
      * 创建时间
      */
     private LocalDateTime createTime;
-    /**
-     * 创建人
-     */
-    private Long createUser;
+
     /**
      * 更新时间
      */
     private LocalDateTime updateTime;
-    /**
-     * 更新人
-     */
-    private Long updateUser;
+
 }
