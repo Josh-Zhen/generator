0463861ebb731ae2be93e9258d83af4696ef3665
by丶Moonlit/2 <by.Moonlit@gmail.com>
2021-11-18 10:33:32 +0800

    1.修复时间展示问题

--- src/main/java/com/moonlit/generator/system/entity/DictType.java
+++ src/main/java/com/moonlit/generator/system/entity/DictType.java
@@ -1,5 +1,7 @@
 package com.moonlit.generator.system.entity;
 
+import cn.hutool.core.date.DatePattern;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -44,18 +46,20 @@ public class DictType implements Serializable {
     private String remark;
 
     /**
-     * 状态（字典 0停用 1正常 2删除）
+     * 状态（0停用 1正常）
      */
     private Integer status;
 
     /**
      * 创建时间
      */
+    @JsonFormat(timezone = "GMT+8", pattern = DatePattern.NORM_DATETIME_PATTERN)
     private LocalDateTime createDate;
 
     /**
      * 更新时间
      */
+    @JsonFormat(timezone = "GMT+8", pattern = DatePattern.NORM_DATETIME_PATTERN)
     private LocalDateTime updateDate;
 
 }
